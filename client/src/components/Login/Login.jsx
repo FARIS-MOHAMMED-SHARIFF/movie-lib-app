b33e490aa5b3d3b1e7129672e180f91dec2ed0d5
Faris <itsfaris05@gmail.com>
2022-06-02 12:36:01 +0530

    update in the server

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/login";
+			const url = "/login";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			window.location = "/";
